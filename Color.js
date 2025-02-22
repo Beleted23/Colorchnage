ea415a3b6d9666c13ed273c41db701fdfccfadc5
Beleted23 <duchishobelete1@gmail.com>
2025-02-21 16:24:08 -0800

    Colorchange

---
+++ Color.js
@@ -0,0 +1,12 @@
+document.getElementById("red").addEventListener('click', function(){
+  document.body.style.backgroundColor ="red";
+})
+document.getElementById("yellow").addEventListener('click', function(){
+  document.body.style.backgroundColor ="yellow";
+})
+document.getElementById("green").addEventListener('click', function(){
+  document.body.style.backgroundColor ="green";
+})
+document.getElementById("blue").addEventListener('click', function(){
+  document.body.style.backgroundColor ="blue";
+})
